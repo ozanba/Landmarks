27a1afb7595daa65a7a0f49e0e9cb8b603b00acc
ozanba <ozanbagiran@gmail.com>
2024-01-21 12:12:38 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ozan Bağıran on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
